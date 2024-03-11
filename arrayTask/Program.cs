a3492a2d2885ed21acd4fa2528e6ba757e1d34d4
“NijatMajidov” <bd7u6h2en@code.edu.az>
2024-03-11 14:50:17 +0400

    Edit code

--- arrayTask/Program.cs
+++ arrayTask/Program.cs
@@ -5,16 +5,18 @@
         static void Main(string[] args)
         {
             int[] array = { 25, 16, 75, 82, 57 };
-            int sum = 0;
+            int sum = 0,hasil=1;
 
             for (int i = 0; i < array.Length; i++)
             {
                 if (i % 2 == 0)
                 {
+                    hasil*=array[i];
                     sum+=array[i];
                 }
             }
-            Console.WriteLine(sum);
+            Console.Write(sum);
+            Console.WriteLine("  "+ hasil);
         }
     }
 }
