2116485df999013b9450e58446f87eecbda8dbbb
“NijatMajidov” <bd7u6h2en@code.edu.az>
2024-03-11 14:48:59 +0400

    Add project files.

---
+++ arrayTask/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace arrayTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 25, 16, 75, 82, 57 };
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum+=array[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
