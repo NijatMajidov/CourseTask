be280b091fefe56a324f68d20ff9b50b71114824
“NijatMajidov” <bd7u6h2en@code.edu.az>
2024-03-15 18:33:26 +0400

    Add method tasks

---
+++ Method/MethodTask1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MethodTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char  simvol= 'A';
+            string word = "Salam Admin";
+            Console.WriteLine(Check(word, simvol)); 
+        }
+
+        public static bool Check (string word, char herf)
+        {
+            for(int i = 0; i < word.Length; i++)
+            {
+                if(word[i] == herf)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Method/MethodTask2/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace MethodTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Salam, necesiz";
+            FirstLetter(word);
+        }
+
+        //public static void Area(double r)
+        //{
+        //    int p = 3;
+        //    Console.WriteLine(p*(r*r));
+
+        //}
+        //public static void Area(double a, double b)
+        //{
+        //    Console.WriteLine(a*b);
+
+        //}
+        //public static void Area(double a, double b, double c)
+        //{
+        //    Console.WriteLine(2*(a*b+a*c+b*c));
+
+        //}
+        //public static void Area(double r, double a, double b, double c)
+        //{
+        //    double p = (a + b + c) / 2;
+        //    Console.WriteLine(p*r);
+        //}
+
+        //public static string DelSpace(string word)
+        //{
+        //    string newWord=" ";
+        //    for(int i=0; i < word.Length; i++)
+        //    {
+        //        if(word[i] !=' ')
+        //        {
+        //            newWord += word[i];
+        //        }
+        //    }
+        //    return newWord;
+        //}
+
+        public static void FirstLetter(string word)
+        {
+            string first = " ";
+           if (word[0]!= ' ')
+            {
+                first += word[0];
+            }
+            for (int i = 0; i < word.Length-1; i++)
+            {
+
+                    if (word[i] == ' '&& word[i+1]!=' ')
+                    {
+                        
+                        first +=" "+word[i+1];
+                       
+                    } 
+            }
+            Console.WriteLine(first); 
+        }
+
+        //public static void Calculator(double num, char operant, double num2)
+        //{
+
+        //    switch (operant)
+        //    {
+        //        case '+':
+        //            Console.WriteLine(num + num2);
+        //            break;
+        //        case '-':
+        //            Console.WriteLine(num - num2);
+        //            break;
+        //        case '*':
+        //            Console.WriteLine(num * num2);
+        //            break;
+        //        case '/':
+        //            Console.WriteLine(num / num2);
+        //            break;
+        //        default:
+        //            Console.WriteLine("emeliyat yoxdur");
+        //            break;
+        //    }
+        //}
+    }
+}
