abd5dd6574b17ee380544d7d333ff7be9507f7e8
“NijatMajidov” <bd7u6h2en@code.edu.az>
2024-03-27 15:54:57 +0400

    Class tasks

---
+++ Class/ClassTask1/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask1
+{
+    internal class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public double CurrentFuel { get; set; }
+        public double FuelFor1Km { get; set; }
+        public double Millage { get; set; }
+        public Car(string brand, string model, double currentFuel, double fuelFor1Km, double millage)
+        {
+            Brand = brand;
+            Model = model;
+            CurrentFuel = currentFuel;
+            FuelFor1Km = fuelFor1Km;
+            Millage = millage;
+        }
+
+        public void Drive(int km)
+        {
+            if (CurrentFuel >= FuelFor1Km * km)
+            {
+                Millage+=km;
+                CurrentFuel -= km * FuelFor1Km;
+                Console.WriteLine($"Millage:{Millage} Fuel:{CurrentFuel}");
+            }
+            else
+            {
+                Console.WriteLine("Kifayet qeder benzin yoxdur");
+            }
+        }
+    }
+}
---
+++ Class/ClassTask1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car newCar = new Car("Lada", "niva", 100, 10, 5000);
+
+            newCar.Drive(5);
+        }
+
+    }
+}
