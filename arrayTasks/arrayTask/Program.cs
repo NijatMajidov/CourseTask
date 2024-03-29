f3690dccbfd8038099ab9552975c3d3f1b469c5d
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-03-29 14:24:43 +0400

    Update folder name

---
+++ arrayTasks/arrayTask/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace arrayTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 25, 16, 75, 82, 57 };
+            int sum = 0,hasil=1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    hasil*=array[i];
+                    sum+=array[i];
+                }
+            }
+            Console.Write(sum);
+            Console.WriteLine("  "+ hasil);
+        }
+    }
+}
---
+++ arrayTasks/arrayTask4/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace arrayTask4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            //string[] words = { "lorem", "impus" };
+            //string axtarilan = "impus";
+
+            //for(int i = 0; i < words.Length; i++)
+            //{
+            //    if(words[i] == axtarilan)
+            //    {
+            //        Console.WriteLine(words[i]);    
+            //    }
+            //}
+            #endregion
+
+            #region task2
+            //string word = "salam ";
+            //char herf = 'm';
+            //bool test = false;
+            //for(int i = 0; i < words.Length; i++)
+            //{
+            //    if (herf == word[i])
+            //    {
+            //        test = true;
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine(test);
+            #endregion
+
+            #region task3
+            string[] words = { "salam", "hello", "world" };
+            
+            for(int i = 0; i < words.Length; i++)
+            {
+                for (int j=words[i].Length-1; j >=0 ; j--)
+                {
+                    Console.Write(words[i][j]);
+                }
+                Console.Write(" ");
+            }
+            #endregion
+
+            #region task4
+            //string[] words = { "salam", "hello", "world" };
+            //string[] revs = new string[words.Length];
+            //for(int i = words.Length-1; i >=0 ; i--)
+            //{
+                    //test[words.Length-1-i]=words[i];
+            //        Console.Write(words[i]+" ");
+
+            //}
+            #endregion
+
+        }
+    }
+}
