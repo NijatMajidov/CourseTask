21044eedb53114118826addd30b00f1114b1a1ed
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-04-01 22:45:32 +0400

    Tasks for weapon class completed

---
+++ NewTask/NewTask/Program.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualBasic;
+
+namespace NewTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte tutum = 50;
+            byte gullesayi = 50;
+
+            Weapon weapon = new Weapon(tutum, gullesayi);
+
+            string choice;
+            bool check = true;
+            do
+            {
+                Console.WriteLine("\n--------------------------------------------------------\n" +
+                    "Etmek istediyiniz emeliyeti secin\n " +
+                    "0 - Informasiya \n" +
+                    "1 - Shoot \n" +
+                    "2 - Remain Bullet Count \n" +
+                    "3 - Reload \n" +
+                    "4 - Change Fire Mode \n" +
+                    "5 - Exit \n" +
+                    "6 - Edit \n" +
+                    "\n----------------------------------------------------------\n");
+
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "0":
+                        weapon.ShowWeaponInfo();
+                        break;
+
+                    case "1":
+                        weapon.Shoot();
+                        break;
+
+                    case "2":
+                        Console.WriteLine(weapon.GetRemainBulletCount());
+                        break;
+
+                    case "3":
+                        weapon.Reload();
+                        break;
+
+                    case "4":
+                        weapon.ChangeFireMode();
+                        break;
+
+                    case "5":
+                        Console.WriteLine("\n Program dayandirildi! ");
+                        check = false;
+                        break;
+
+                    case "6":
+                        bool exit = true;
+                        do
+                        {
+                            Console.WriteLine("\n----------------------------------------------------------\n" +
+                                     "\n Edit etmek istediyinizi secin \n " +
+                                     "7 - Tutum deyismesi \n" +
+                                     "8 - Gulle sayi deyisin \n" +
+                                     "9 - Ana Menuye qayit" +
+                                     "\n----------------------------------------------------------\n");
+
+                            string edit = Console.ReadLine();
+
+                            switch (edit)
+                            {
+                                case "7":
+                                    byte newTutum;
+                                    do
+                                    {
+                                        Console.WriteLine("Yeni tutumu daxil edin: \n");
+                                    } while (!byte.TryParse(Console.ReadLine(), out newTutum) && newTutum >= 0);
+                                    weapon.Tutum = newTutum;
+                                    break;
+
+                                case "8":
+                                bullet:
+                                    byte gulle;
+                                    do
+                                    {
+                                        Console.WriteLine("Yeni gulle sayi daxil edin: \n");
+                                    } while (!byte.TryParse(Console.ReadLine(), out gulle) && gulle >= 0);
+                                    if (gulle <= weapon.Tutum) weapon.Hazirkigulle = gulle;
+                                    else
+                                    {
+                                        Console.WriteLine("Duzgun say daxil edin!!");
+                                        goto bullet;
+                                    }
+                                    break;
+
+                                case "9":
+                                    Console.WriteLine("\n Menyuya qayitdiq ");
+                                    exit = false;
+                                    break;
+
+                                default:
+                                    Console.WriteLine(" Bu secim yoxdur! ");
+                                    break;
+                            }
+                        } while (exit);
+                        break;
+                    default :
+                        Console.WriteLine("\n Bu secim yoxdur! \n");
+                        break;
+                }
+            } while (check);
+        }
+    }
+}
---
+++ NewTask/NewTask/Weapon.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTask
+{
+    internal class Weapon
+    {
+        public byte Tutum {  get; set; }
+        public byte Hazirkigulle {  get; set; }
+        public bool Mood;
+       
+            
+
+
+        public Weapon(byte tutum, byte hazirkigulle)
+        {
+            Tutum = tutum;
+            Hazirkigulle = hazirkigulle;
+        }
+
+        public void ShowWeaponInfo()
+        {
+            Console.WriteLine($"Silahin tutumu: {this.Tutum}\n Hazirki gulle sayi: {this.Hazirkigulle}");
+            if (Mood) Console.WriteLine("Hazirki atis modu: Avtomatik");
+            else Console.WriteLine("Hazirki atis modu: Tekli");
+            
+
+        }
+        public void Shoot()
+        {
+            if (Hazirkigulle >  0)
+            {
+                if (!Mood)
+                {
+                    Hazirkigulle--;
+                    Console.WriteLine($"Ates edildi! \n Silah avtomatikde idiSilah teklide idi! \n");
+                }
+                else 
+                {
+                    Hazirkigulle = 0;
+                    Console.WriteLine($"Butun gulleler ateslendi! \n !");
+                }
+            }
+            else Console.WriteLine("Silah bosdur!");
+
+
+        }
+        
+        
+
+        public byte GetRemainBulletCount()
+        {
+            if (Tutum == Hazirkigulle) Console.WriteLine("Silah tam doludur! ");
+            else Console.WriteLine("Daragin tam dolmasi ucun lazimli  gulle sayi: ");
+            return (byte)(Tutum - Hazirkigulle);
+        }
+
+        public void Reload()
+        {
+            if (Hazirkigulle < Tutum)
+            {
+                Hazirkigulle = Tutum;
+                Console.WriteLine("Silah dolduruldu! \n");
+            }
+            else Console.WriteLine("Silah zaten doludur!\n");
+        }
+
+        public void ChangeFireMode()
+        {
+            Mood = !Mood;
+            if (Mood) Console.WriteLine(" Atis modu Teklidenden Avtomatike deyisdi");
+            else Console.WriteLine("Atis modu Avtomatikden Tekliye deyisdi");
+        }
+
+
+    }
+}
