22daf488131776715e6b8b12662caa48e0f589d6
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-04-05 23:29:15 +0400

    Interface related task

---
+++ Interface/Account/Account/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Account
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("user","user@gmail.com");
+
+            user.PasswordChecker("dggargd1223");
+
+            user.ShowInfo();
+        }
+    }
+}
---
+++ Interface/Account/Account/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    internal class User: IAccount 
+    {
+        private int _id;
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public User(string name, string email)
+        {
+            _id++;
+            Id = _id;
+            FullName = name;
+            Email = email;
+        }
+
+        public void PasswordChecker(string password)
+        {
+            if( password.Length>=8 )
+            {
+                bool numChck = false;
+                bool lowChck = false;
+                bool upChck = false;
+
+                for( int i = 0; i < password.Length; i++ )
+                {
+                    if (char.IsDigit(password[i])) numChck = true; 
+
+                    if (char.IsUpper(password[i])) upChck = true;
+
+                    if (char.IsLower(password[i])) lowChck = true; 
+
+                    }
+
+                Console.WriteLine(numChck && lowChck && upChck);
+
+            }
+            }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Id} {FullName} {Email}");
+        }
+        
+    } 
+}
